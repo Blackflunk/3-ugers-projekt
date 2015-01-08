2be4f5216dc0d7846e8c3049e6ccd34cbf5a87c0
Blackflunk <Alexander12350@hotmail.com>
2015-01-08 12:04:27 +0100

    Chancekort delvist lavet, gameboard rettet til Chancekort.

--- src/fields/ChanceCard.java
+++ src/fields/ChanceCard.java
@@ -1,5 +1,23 @@
 package fields;
 
-public class ChanceCard {
+import game.Player;
+
+public class ChanceCard extends Fields{
+	private String name;
+	
+	public ChanceCard(String name){
+		super(name);
+	}
+
+	@Override
+	public void landOnField(Player player){
+	
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -10,7 +10,7 @@ import fields.Tax;
 import fields.Territory;
 import fields.Jail;
 import fields.MoveToJail;
-
+import fields.ChanceCard;
 
 public class GameBoard {
 
@@ -22,12 +22,12 @@ public class GameBoard {
 		field = new Fields[32];
 		field[0] = new Start("Start");
 		field[1] = new Territory("Rï¿½dovrevej",1200, 1000, 600, 40, 200, 600, 1800, 3200, 5000);
-		//field[2] = prï¿½v lykken
+		field[2] = new ChanceCard("Chancekort");
 		field[3] = new Territory("Hvidovrevej", 1200, 1000, 600, 80, 400, 1200, 3600, 6400, 9000);
 		field[4] = new Tax("Statsskat", 2000);
 		field[5] = new Fleet("Helsingï¿½r-Helsingborg", 4000, 2000, 500, 1000, 2000, 4000);
 		field[6] = new Territory("Roskildevej", 2000, 1000, 1000, 120, 600, 1800, 5400, 8000, 11000);
-		//field[7] = prï¿½v lykken
+		field[7] = new ChanceCard("Chancekort");
 		field[8] = new Territory("Valby Langgade", 2000, 1000, 1000, 120, 600, 1800, 5400, 8000, 1100);
 		field[9] = new Territory("Allï¿½gade", 2400, 1000, 1200, 160, 800, 2000, 6000, 9000, 12000);
 		field[10] = new Jail("Fængsel");
@@ -37,12 +37,12 @@ public class GameBoard {
 		field[14] = new Territory("Gl Kongevej", 3200, 2000, 1600, 240, 1200, 3600, 10000, 14000, 18000);
 		field[15] = new Fleet("Mols-Linien", 4000, 2000, 500, 1000, 2000, 4000);
 		field[16] = new Territory("Bernstorffsvej", 3600, 2000, 1800, 280, 1400, 4000, 11000, 15000, 19000);
-		//field[17] =	prï¿½v lykken
+		field[17] = new ChanceCard("Chancekort");
 		field[18] = new Territory("Hellerupvej", 3600, 2000, 1800, 280, 1400, 4000, 11000, 15000, 19000);
 		field[19] = new Territory("Strandvejen", 4000, 2000, 2000, 320, 1600, 4400, 12000, 16000, 20000);
 		field[20] = new Refuge("Helle", 5000);
 		field[21] = new Territory("Trianglen", 4400, 3000, 2200, 360, 1800, 5000, 14000, 17500, 21000);
-		//field[22] = prï¿½v lykken
+		field[22] = new ChanceCard("Chancekort");
 		field[23] =	new Territory("ï¿½sterbrogade", 4400, 3000, 2200, 360, 1800, 5000, 14000, 17500, 21000);
 		field[24] = new Territory("Grï¿½nningen", 4800, 3000, 2400, 400, 2000, 6000, 15000, 18500, 22000);
 		field[25] = new Fleet("Gedser-Rostock", 4000, 2000, 500, 1000, 2000, 4000);
@@ -53,10 +53,10 @@ public class GameBoard {
 		field[30] = new MoveToJail("Gå I Fængslet");
 		field[31] = new Territory("Amagertorv", 6000, 4000, 3000, 520, 2600, 7800, 18000, 22000, 25500);
 		field[32] =	new Territory("Vimmelskaftet", 6000, 4000, 3000, 520, 2600, 7800, 18000, 22000, 25500);
-		//field[33] = prï¿½v lykken
+		field[33] = new ChanceCard("Chancekort");
 		field[34] = new Territory("Nygade", 6400, 4000, 3200, 560, 3000, 9000, 20000, 24000, 28000);
 		field[35] = new Fleet("Rï¿½dby-Puttgarden", 4000, 2000, 500, 1000, 2000, 4000);
-		//field[36] = prï¿½v lykken
+		field[36] = new ChanceCard("Chancekort");
 		field[37] = new Territory("Frederiksberggade", 7000, 4000, 3500, 700, 3500, 10000, 22000, 26000, 30000);
 		field[38] = new Tax("Indkomstskat", 4000);
 		field[39] =	new Territory("Rï¿½dhuspladsen", 8000, 4000, 4000, 1000, 4000, 12000, 28000, 34000, 40000);
