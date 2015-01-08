e016b45ac14e1a9caa6fce317ac690d54b4e6d0c
Blackflunk <Alexander12350@hotmail.com>
2015-01-08 13:36:55 +0100

    Rettede kommentarer.

--- src/JUnit_Tests/PastStartTest.java
+++ src/JUnit_Tests/PastStartTest.java
@@ -16,33 +16,33 @@ public class PastStartTest {
 	GameBoard gameboard = new GameBoard(null);
 	Player player1 = new Player("Spiller1");
 	//Test
-		//S�tter ham p� position 37.
+		//Set the player position to field 37.
 		player1.setPosition(37);
-		//Rykker ham over start ved at sl� 5.
+		//Move player 5 positions forward.
 		player1.movePosition(5);
 	//Postconditions
-		//Tjekker om han er p� position 2.
+		//Check if position 2
 		assertEquals(2,player1.getPosition());
 	}
 	
 	@Test
 	public void test2(){
 	//Preconditions
-		//Opretter gameboard og spiller
+		//Initialise the gameboard and player object.
 		GameBoard gameboard = new GameBoard(null);
 		Player player1 = new Player("Spiller1");
-		//S�tter spillerens scorer til 5000
+		//Set the player score to 5000.
 		player1.account.setScore(5000);
 	
 	//Test
-		//S�tter ham p� position 37.
+		//Set the player position to field 37.
 		player1.setPosition(37);
-		//Rykker ham over start ved at sl� 5.
+		//Move player 5 positions forward.
 		player1.movePosition(5);
 	//Postconditions
-		//Tjekker om han er p� position 2.
+		//Check if position 2
 		assertEquals(2,player1.getPosition());
-		//Tjekker om man har modtaget penge over start.
+		//Check if player received 4000 when crossing start.
 		assertEquals(9000,player1.account.getScore());
 	}
 	
