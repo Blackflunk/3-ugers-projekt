ca8d3a5981187feda59015a7a6b85de535aeb707
Blackflunk <Alexander12350@hotmail.com>
2015-01-08 13:38:19 +0100

    Rettelse af kommentarer.

--- src/JUnit_Tests/MoveToJailTest.java
+++ src/JUnit_Tests/MoveToJailTest.java
@@ -13,17 +13,17 @@ public class MoveToJailTest {
 	public void testFaengsel(){
 	
 	//Preconditions
-		//opretter spiller
+		//Initialise gameboard and player objects.
 	GameBoard gameboard = new GameBoard(null);
 	Player player1 = new Player("Spiller1");
 	
 	//Test
-		// lader spilleren lande på felt 31 "gå i fængsel"
+		// Sets the players position to 30.
 	player1.setPosition(30);
 	gameboard.getField(player1.getPosition()).landOnField(player1);
 
 	//Postconditions
-		// Tjekker at spilleren er i fængsel
+		// Check if it is true that the player is now in jail.
 	assertEquals(true, player1.isJailed());
 	}
 }
