7b9b0b5436245ae475af2987ced9d4b982d2ed9b
Dtu Andreas <s082853@student.dtu.dk>
2015-01-14 13:23:44 +0100

    Stavefejl

--- src/control/TurnController.java
+++ src/control/TurnController.java
@@ -71,7 +71,7 @@ public class TurnController {
 			run = false;
 			playerlist[currentPlayer].setJailed(true);
 			playerlist[currentPlayer].setPosition(10);
-			GUIC.showMessage("Du 3 par i træk, du ryger i fængsel");
+			GUIC.showMessage("Du har slået 3 par i træk, du ryger i fængsel");
 			GUIC.newPositon(playerlist[currentPlayer]);
 		}
 		}
@@ -185,7 +185,7 @@ public class TurnController {
 			run = false;
 			playerlist[currentPlayer].setJailed(true);
 			playerlist[currentPlayer].setPosition(10);
-			GUIC.showMessage("Du 3 par i træk, du ryger i fængsel");
+			GUIC.showMessage("Du har slået 3 par i træk, du ryger i fængsel");
 			GUIC.newPositon(playerlist[currentPlayer]);
 		}
 		}
