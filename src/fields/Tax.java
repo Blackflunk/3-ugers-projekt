d328c74064739045e456623aeeecffd967fd0e77
AoschkA <jonaspraem@gmail.com>
2015-01-14 15:54:57 +0100

    Tax rettet

--- src/fields/Tax.java
+++ src/fields/Tax.java
@@ -7,49 +7,58 @@ public class Tax extends Field {
 
 	private String name;
 	private int pay;
-	private GUIcontroller out = new GUIcontroller();
+	private boolean option;
+	private boolean paypercent;
 
-	public Tax(String name, int pay) {
+	public Tax(String name, int pay, boolean option) {
 		super(name);
 		this.pay = pay;
 		this.name = name;
+		this.option = option;
 	}
 
 	@Override
 	public void landOnField(Player player) {
 		// If a player lands on goldmine
-		if (name.equals("Statsskat")) {
-			out.goldmineMessage(name);
+		if (option) {
+			if (paypercent) {
+				player.account.addPoints(-player.account.getScore() / 10);
+			}
+			else{
+			// out.goldmineMessage(name);
 			if (player.account.getScore() >= pay) {
 				player.account.addPoints(-pay);
 			} else {
-				out.insufficiantFundsTax();
+				// out.insufficiantFundsTax();
 				player.account.addPoints(-player.account.getScore());
 				player.setStatus(true);
+			}
 			}
 		}
 		// If a player lands on caravan
 		else {
-			String taxPick = out.taxPick(name);
-			if (taxPick == "4000") {
-				if (player.account.getScore() >= pay) {
-					player.account.addPoints(-pay);
-				} else {
-					out.taxFunds();
-					player.account.addPoints(-player.account.getScore());
-					player.setStatus(true);
-				}
+			if (player.account.getScore() >= pay) {
+				player.account.addPoints(-pay);
 			} else {
-				player.account.addPoints((int) -player.account.getScore() / 10);
+				// out.insufficiantFundsTax();
+				player.account.addPoints(-player.account.getScore());
+				player.setStatus(true);
 			}
-
 		}
-		out.updateBalance(player.getName(), player.account.getScore());
+		// out.updateBalance(player.getName(), player.account.getScore());
 
 	}
 	@Override
 	public String toString() {
 		return "Type: Tax --- Name: " + name + " --- Tax: " + pay + "\n";
+	}
+
+	public boolean isPaypercent() {
+		return paypercent;
+	}
+
+	public void setPaypercent(boolean paypercent) {
+		this.paypercent = paypercent;
 	}
 
 }
