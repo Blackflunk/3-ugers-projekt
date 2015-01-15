3a097872182cae98bf66d0b1e44a9bc82aad3c31
AoschkA <jonaspraem@gmail.com>
2015-01-15 12:29:49 +0100

    indsat ny launch

---
+++ src/run/launch.java
@@ -0,0 +1,10 @@
+package run;
+import control.GameController;
+
+public class launch {
+	public static void main(String[] args) {
+		GameController game = new GameController();
+		game.init();
+	}
+
+}
