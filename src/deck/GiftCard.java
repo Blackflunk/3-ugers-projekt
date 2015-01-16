b8416627b3afb88f3edd63f8c75a991748b0cdab
AoschkA <jonaspraem@gmail.com>
2015-01-16 14:12:41 +0100

    GiftCard

--- src/deck/GiftCard.java
+++ src/deck/GiftCard.java
@@ -13,9 +13,9 @@ public class GiftCard extends RecieveCard {
 		for (int i=0; i < playerlist.length; i++) {
 			boolean t = player.equals(playerlist[i]);
 			if (!t) {
-				if (player.account.getScore()<playerlist[i].account.getScore()){
-					player.setStatus(true);
-					player.account.setScore(0);
+				if (bonus>playerlist[i].account.getScore()){
+					playerlist[i].setStatus(true);
+					playerlist[i].account.setScore(0);
 				}		
 				else
 				playerlist[i].account.addPoints(-bonus);
